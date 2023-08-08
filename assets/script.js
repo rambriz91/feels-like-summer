4d41619ca7c424f3040e8d05a3c30b75d220c6e1
rambriz91 <rambriz91@gmail.com>
2023-08-08 13:30:38 -0700

    consolided code.

--- assets/script.js
+++ assets/script.js
@@ -16,6 +16,22 @@ var weekday = [
     moment().add(6, 'd').format('ddd MMM Do'),
 ];
 
+function displayWeather(data) {
+    display.children[0].textContent = data.city.name;
+
+    document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?landscape?` + userInput.value + `')`;
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style.backgroundSize = 'cover';
+
+    for (let i = 0; i < card.length; i++) {
+        card[i].children[0].textContent = weekday[i];
+        card[i].children[1].src = 'https://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '.png';
+        card[i].children[2].children[0].textContent = '🌡️ ' + data.list[i].main.temp.toFixed(0) + '°F';
+        card[i].children[2].children[1].textContent = '🌬️ ' + data.list[i].wind.speed.toFixed(0) + ' mph';
+        card[i].children[2].children[2].textContent = '💦 ' + data.list[i].main.humidity + '%';
+    }
+}
+
 function getWeather() {
     var cityByName = 'https://api.openweathermap.org/geo/1.0/direct?q=' + userInput.value + '&limit=1&appid=' + APIKey +'';
     fetch(cityByName)
@@ -31,21 +47,7 @@ function getWeather() {
                     return response.json();
                 })
                 .then(function (data) {
-                    console.log(data);
-                    document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?landscape?` + userInput.value + `')`;
-                    document.body.style.backgroundRepeat = 'no-repeat';
-                    document.body.style.backgroundSize = 'cover';
-
-                    display.children[0].textContent = data.city.name;
-
-                    for (let i = 0; i < card.length; i++) {
-                        card[i].children[0].textContent = weekday[i];
-                        card[i].children[1].src = 'https://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '.png';
-                        card[i].children[2].children[0].textContent = '🌡️ ' + data.list[i].main.temp.toFixed(0) + '°F';
-                        card[i].children[2].children[1].textContent = '🌬️ ' + data.list[i].wind.speed.toFixed(0) + ' mph';
-                        card[i].children[2].children[2].textContent = '💦 ' + data.list[i].main.humidity + '%';
-                    }
-
+                    displayWeather(data);
                 })
         })
 }
@@ -64,20 +66,7 @@ window.addEventListener('load', function () {
                     return response.json()
                 })
                 .then(function (data){
-                    display.children[0].textContent = data.city.name;
-
-                    document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?landscape?` + userInput.value + `')`;
-                    document.body.style.backgroundRepeat = 'no-repeat';
-                    document.body.style.backgroundSize = 'cover';
-
-                    for (let i = 0; i < card.length; i++) {
-                        card[i].children[0].textContent = weekday[i];
-                        card[i].children[1].src = 'https://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '.png';
-                        card[i].children[2].children[0].textContent = '🌡️ ' + data.list[i].main.temp.toFixed(0) + '°F';
-                        card[i].children[2].children[1].textContent = '🌬️ ' + data.list[i].wind.speed.toFixed(0) + ' mph';
-                        card[i].children[2].children[2].textContent = '💦 ' + data.list[i].main.humidity + '%';
-                    }
-
+                displayWeather(data);
                 })
         })
     }
